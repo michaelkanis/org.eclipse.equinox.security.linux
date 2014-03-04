561ff0f0c8dd253229ee178eaa6082383a710e3b
Michael Kanis <mks@skweez.net>
2014-03-04 23:06:58 +0100

    Use generated makefiles to build the lib

--- plugin/native/test.c
+++ plugin/native/test.c
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
 
-#if TEST
+#ifdef TEST
 
 #include "secretServiceDBus.h"
 #include <sys/types.h>
